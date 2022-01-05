6bf2cefb770293c2d75346aa93609a535ad1be21
cty123 <ctychen2216@gmail.com>
2022-01-04 23:21:58 -0800

    Properly configure certificate on client side.

--- src/config/tls.rs
+++ src/config/tls.rs
@@ -71,9 +71,18 @@ pub fn make_client_config(config: &OutboundTlsConfig) -> Arc<ClientConfig> {
 
         Arc::new(config)
     } else {
+        let mut root_store = RootCertStore::empty();
+        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+            rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        }));
+
         let config = ClientConfig::builder()
             .with_safe_defaults()
-            .with_root_certificates(RootCertStore::empty())
+            .with_root_certificates(root_store)
             .with_no_client_auth();
 
         Arc::new(config)
--- src/proxy/handler.rs
+++ src/proxy/handler.rs
@@ -1,7 +1,6 @@
 use log::info;
 use rustls::ServerName;
 use sha2::{Digest, Sha224};
-use tokio::runtime::Handle;
 use std::io::{Error, ErrorKind, Result};
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
